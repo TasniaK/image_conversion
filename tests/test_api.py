6f7ac226d9b2008060ee350269cc45b0f2fd1a0e
Tasnia <tasniakamal@hotmail.co.uk>
2019-08-20 23:19:45 +0100

    Add docstrings to tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,6 +8,7 @@ CORRECT_FILE = './tests/test_bunny.jpg'
 INCORRECT_FILE = './tests/test_bunny_wrong_extension.j'
 
 def upload_test_image(image_filename, client):
+    """Upload a given image file through api with test client."""
     with open(image_filename, 'rb') as image_file:
         image = io.BytesIO(image_file.read())
     data = {'file': (image, image_filename)}
@@ -16,15 +17,18 @@ def upload_test_image(image_filename, client):
     return response
 
 def test_upload_image(client):
+    """Test if an image can be uploaded via the api."""
     response = upload_test_image(CORRECT_FILE, client)
     assert response.status_code == 200
 
 def test_has_allowed_ext(client):
+    """Test if incorrect image extensions raise an error."""
     response = upload_test_image(INCORRECT_FILE, client)
     print(response.data)
     assert response.status_code == 400
 
 def test_get_image_original(client):
+    """Test if an image can be returned."""
     upload_response = upload_test_image(CORRECT_FILE, client)
     identifier = ((upload_response.data).decode("utf-8")).split(': ')[1]
 
@@ -33,6 +37,7 @@ def test_get_image_original(client):
     assert response.status_code == 200
 
 def test_get_image_converted(client):
+    """Test if a converted image can be returned using a given format."""
     upload_response = upload_test_image(CORRECT_FILE, client)
     identifier = ((upload_response.data).decode("utf-8")).split(': ')[1]
 
@@ -40,6 +45,3 @@ def test_get_image_converted(client):
     response = client.get('image/{0}/?={1}'.format(identifier, extension))
 
     assert response.status_code == 200
-
-#get_image(image_identifier) original
-#get_image(image_identifier) convert
