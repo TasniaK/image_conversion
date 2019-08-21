069cfd31d0fd5f96c02bd6f0550ec0d95c7dc56e
Tasnia <tasniakamal@hotmail.co.uk>
2019-08-21 16:34:39 +0100

    added tests checking if image_save gets called or not

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,7 @@
 import io
-import random
-from config import ALLOWED_EXTENSIONS
+from mock import patch, Mock
+from prog_image import image_save
+import os
 
 # Use client created in conftest to carry out requests.
 
@@ -39,9 +40,36 @@ def test_get_image_original(client):
 def test_get_image_converted(client):
     """Test if a converted image can be returned using a given format."""
     upload_response = upload_test_image(CORRECT_FILE, client)
+    extension = 'png'
     identifier = ((upload_response.data).decode("utf-8")).split(': ')[1]
 
-    extension = random.choice(list(ALLOWED_EXTENSIONS))
-    response = client.get('image/{0}/?={1}'.format(identifier, extension))
+    response = client.get('image/{0}/?format={1}'.format(identifier, extension))
 
     assert response.status_code == 200
+
+def test_get_image_converted(client):
+    """Test if a converted image hits the image_save() function."""
+    upload_response = upload_test_image(CORRECT_FILE, client)
+    identifier = ((upload_response.data).decode("utf-8")).split(': ')[1]
+    extension = 'png'
+
+    # if converted file already exists, remove
+    if os.path.exists('./uploaded_images/{0}.{1}'.format(identifier, extension)):
+        os.remove('./uploaded_images/{0}.{1}'.format(identifier, extension))
+
+    with patch('prog_image.image_save', side_effect=image_save) as mock:
+        client.get('image/{0}/?format={1}'.format(identifier, extension))
+        assert mock.call_args_list[0].args[1] == '{0}.{1}'.format(identifier, extension)
+        assert mock.call_args_list[0].args[2] == extension
+
+def test_get_image_format_same_as_original(client):
+    """Test if image format given is same as original format, image is not converted."""
+    upload_response = upload_test_image(CORRECT_FILE, client)
+    identifier = ((upload_response.data).decode("utf-8")).split(': ')[1]
+    extension = 'jpg'
+
+    client.get('image/{0}/?={1}'.format(identifier, extension))
+    mock = Mock()
+    function = mock.image_save()
+    assert not function.called
+    
